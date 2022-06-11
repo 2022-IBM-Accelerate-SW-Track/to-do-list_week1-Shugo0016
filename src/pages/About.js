2df07dc5b4fe6f7acec40808bc47327783faaffa
Shugo0016 <93568520+Shugo0016@users.noreply.github.com>
2022-06-11 13:07:52 -0700

    This is the first submission

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import "./About.css";
+import kaya from "../assets/kaya.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+         <div>
+    <div class="split left">
+      <div className="centered">
+        <img className="kaya" src={kaya} alt="Profile Pic"></img>
       </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Kaya Erol</div>
+        <div className="brief_description">
+          I am a Computer Science Major at New York University.
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+      </div>
+      
     )
   }
 }
